bb4d8c029c3bc318e083b744c9c8d61d4ddb735e
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-08 01:13:03 +0530

    form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -61,9 +61,10 @@ const Signup = () => {
     <input ref={passwordRef} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='enter your password' required />
   </div>
  
-  <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-5">Submit</button>
-  <span className='text-white'>Already have a Account <Link  className='text-blue-800' to={"/"} >Click here</Link>  </span>
+  <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Submit</button>
+  
 </form>
+<span className='text-white'>Already have a Account <Link  className='text-blue-800' to={"/"} >Click here</Link>  </span>
 
 
     </div>
