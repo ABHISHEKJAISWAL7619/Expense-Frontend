20eedfbe504ad515f304979cfdc170d2ce94f055
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-08 01:07:44 +0530

    link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,6 +49,7 @@ const Login = () => {
 <form className="max-w-sm mx-auto w-[500px]">
  
   <div className="mb-5 ">
+  <h1 className='font-bold h-18  dark:text-white  ' >Login page </h1>
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
     <input ref={emailRef} type="email" id="email" className="bg-orange-400 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
   </div>
@@ -58,6 +59,7 @@ const Login = () => {
   </div>
  
   <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <span className='text-white'>Do not have any acoount <Link to={"/register"} >Click here</Link>  </span>
 </form>
 
 
