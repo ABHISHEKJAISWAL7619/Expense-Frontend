54212a0c7e9e6c6518e14ab291cec0bab05ebc12
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-08 01:24:47 +0530

    finalupdate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,28 +43,32 @@ const Login = () => {
   // console.log(obj)  //  {name:"bcd", course:"fullstack", age:76}
 
   return (
-    <div className='h-[100vh] flex items-center bg-slate-900'>
+    <div className='bg-slate-900'>
+      <div className='h-[100vh] flex items-center bg-slate-900'>
        
 
-<form className="max-w-sm mx-auto w-[500px]">
- 
-  <div className="mb-5 ">
-  <h1 className='font-bold h-18  dark:text-white  ' >Login page </h1>
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input ref={emailRef} type="email" id="email" className="bg-orange-400 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-  </div>
-  <div className="mb-5">
-    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-    <input ref={passwordRef} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='enter your password' required />
-  </div>
- 
-  <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-  <span className='text-white'>Do not have any acoount <Link to={"/register"} >Click here</Link>  </span>
-</form>
-
-
+       <form className="max-w-sm mx-auto w-[500px]">
+        
+         <div className="mb-5 ">
+         <h1 className='font-bold h-18  dark:text-white  ' >Login page </h1>
+           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+           <input ref={emailRef} type="email" id="email" className="bg-orange-400 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+         </div>
+         <div className="mb-5">
+           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+           <input ref={passwordRef} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='enter your password' required />
+         </div>
+        
+         <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+         
+       </form>
+       
+       
+           </div>
+           <span className='text-white ml-44'>Already have a Account <Link  className='text-blue-800' to={"/register"} >Click here</Link>  </span>
     </div>
   )
 }
 
 export default Login
+
