460d6bc0507654e13120f83aab43bf10f9df230b
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-08 01:29:32 +0530

    button

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -63,9 +63,9 @@ const Signup = () => {
         </div>
        
         <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Submit</button>
-        
+        <span className='text-white ml-44'>Already have a Account <Link  className='text-blue-800' to={"/"} >Click here</Link>  </span>
       </form>
-      <span className='text-white ml-44'>Already have a Account <Link  className='text-blue-800' to={"/"} >Click here</Link>  </span>
+      
       
       
       
