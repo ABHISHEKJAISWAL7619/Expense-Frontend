c42c0dc59f378bd892ca4f83662748a5191c0550
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-08 00:59:45 +0530

    name

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -42,12 +42,13 @@ const Signup = () => {
 
   return (
     <div className='h-[100vh] flex items-center bg-slate-900'>
-      <h1 className='font-bold  dark:text-white  ' >Sign up form </h1>
+      
     
        
 
 <form className="max-w-sm mx-auto w-[500px]">
   <div className="mb-5">
+  <h1 className='font-bold  dark:text-white  ' >Signup page </h1>
     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
     <input ref={nameRef} type="text" id="name" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-50 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter your name" required />
   </div>
